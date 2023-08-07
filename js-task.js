63c16de28285fb574a134df8e49601693521cba0
isagarjaiswal <sagarjaiswal815555@gmail.com>
2023-08-07 20:27:14 +0530

    day-11 task

---
+++ js-task.js
@@ -0,0 +1,39 @@
+
+
+// const studentMarks = {
+//     math: 80,
+//        science: 75,
+//        history: 70,
+//        english: 65,
+//        art: 72
+//    };
+//    const calculateAverageAndPercentage = (studentMarks) =>{
+//        const {math,science,history,english,art} =studentMarks;
+//        const totalSubjects = 5;
+//        const averageMarks = (math + science + history + english + art)/totalSubjects;
+//        const percentageMarks = averageMarks/80*100 
+//        return {averageMarks, percentageMarks:percentageMarks}
+//    }
+   
+   
+//    const result = calculateAverageAndPercentage(studentMarks);
+//    result
+
+// function covertTemperature(temperature,unit){
+//     switch(unit){
+//         case "C":{
+//          return Math.round( (temperature * 9/5)+32  )
+//             break;
+//         }
+//        case "F":{
+ 
+//           return Math.round((temperature - 32)*5/9 )
+           
+//             break;
+//         }     
+//         default:{
+//                 return `value are not correct`
+//             }
+//     }
+//  }
+//  covertTemperature(98.6,'F')
