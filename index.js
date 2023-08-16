100f7a285edc6682fc830d9fd0507bcb8a3f5fd9
isagarjaiswal <sagarjaiswal815555@gmail.com>
2023-08-16 13:33:00 +0530

    only one question are do

---
+++ index.js
@@ -0,0 +1,18 @@
+const sentence = "The quick brown fox jumps over the lazy dog.";
+
+// Q1: What is the alphabet on index 6 in the given string?
+// console.log(sentence.substring(6,7));   // i
+// console.log(sentence.slice(6,7));   // i
+
+// Q2: What is the alphabet on index 3 in the given string from the end?
+const ind = sentence.length-1-3
+const indi = 1
+// console.log(ind);
+console.log(sentence.substring(ind,indi));   // 
+
+
+// Q3: What will be the character code of the word f, using string method?
+
+// Q4: Use the concat method to create a full name by combining the first name and last name.
+//  Store the result in the variable ‘fullName’ and print it.
+
