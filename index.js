9a2db55ca2bc52dadb916cb85280434ebd406a49
isagarjaiswal <sagarjaiswal815555@gmail.com>
2023-08-23 00:47:24 +0530

    review code are completed withour commets

--- index.js
+++ index.js
@@ -6,17 +6,6 @@ const male = document.getElementById("male");
 const female = document.getElementById("female");
 const errorMsg = document.getElementById("errorMsg");
 
-// function validInput(event) {
-//   if ((event === "age" && age.value < 18) || age.value > 100) {
-//     errorMsg.innerText = "age should not be less than 18";
-//     errorMsg.style = "border:1px solid red";
-//   } else if (event === "phoneNumber" && phoneNumber.value.length !== 10) {
-//     errorMsg.innerText = "Phone number should be 10 digit";
-//     errorMsg.style = "border:1px solid red";
-//   } else {
-//     errorMsg.innerText = "";
-//   }
-// }
 const validateAge = () => {
   errorMsg.innerText =
     age.value < 18 ? "Your Age should be \n greater or equal to 18" : "";
