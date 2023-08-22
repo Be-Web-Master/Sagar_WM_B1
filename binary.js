23184f4ca2431e897b379645d247c961307dcacb
isagarjaiswal <sagarjaiswal815555@gmail.com>
2023-08-22 18:00:15 +0530

    linear Searching

---
+++ binary.js
@@ -0,0 +1,16 @@
+function binary(arr, searchValue) {
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.floor((start + end) / 2);
+  while (arr[middle] !==searchValue && start <= end) {
+    if (arr[middle] < searchValue) start = middle + 1;
+    else end = middle - 1;
+    middle = Math.floor((start + end) / 2);
+    console.log({ start, end, middle });
+  }
+  if (arr[middle] === searchValue) return middle;
+
+  return -1
+}
+
+console.log(binary([11, 22, 33, 44, 55, 66, 77, 88, 99], 9));
