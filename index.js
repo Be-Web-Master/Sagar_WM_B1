4770ba567da12ca12e725aab7acac7a5a3c9b3ea
isagarjaiswal <sagarjaiswal815555@gmail.com>
2023-08-23 00:46:24 +0530

    review code are completed

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 const firstName = document.getElementById("firstName");
 const lastName = document.getElementById("lastName");
 const age = document.getElementById("age");
-const tel = document.getElementById("tel");
+const phoneNumber = document.getElementById("phoneNumber");
 const male = document.getElementById("male");
 const female = document.getElementById("female");
 const errorMsg = document.getElementById("errorMsg");
 
-const styleVariable = "border:1px solid red; padding: 10px;color: rgba(255, 0, 0, 0.292);"
-function validInput(event) {
-  if (event === "age" && age.value < 18 || age.value > 100) {
-    errorMsg.innerText = "age should not be less than 18";
-    errorMsg.style="border:1px solid red";
-  }
-   else if(event === "tel" && tel.value.length !== 10) {
-    errorMsg.innerText = "Phone number should be 10 digit";
-    errorMsg.style="border:1px solid red";
-  } else {
-    errorMsg.innerText = "";
-  }
-}
+// function validInput(event) {
+//   if ((event === "age" && age.value < 18) || age.value > 100) {
+//     errorMsg.innerText = "age should not be less than 18";
+//     errorMsg.style = "border:1px solid red";
+//   } else if (event === "phoneNumber" && phoneNumber.value.length !== 10) {
+//     errorMsg.innerText = "Phone number should be 10 digit";
+//     errorMsg.style = "border:1px solid red";
+//   } else {
+//     errorMsg.innerText = "";
+//   }
+// }
+const validateAge = () => {
+  errorMsg.innerText =
+    age.value < 18 ? "Your Age should be \n greater or equal to 18" : "";
+};
+const validatePhoneNumber = () => {
+  errorMsg.innerText =
+    phoneNumber.value.length !== 10 ? "Number length should be 10" : "";
+};
 
-function submitHandler(event) {
+const onSubmit = (event) => {
   event.preventDefault();
   console.log({
     firstName: firstName.value,
     lastName: lastName.value,
     age: age.value,
-    tel: tel.value,
-    gender: male.checked ? "male" : female.checked ? "female" : "other"
+    phoneNumber: phoneNumber.value,
+    gender: male.checked ? "male" : female.checked ? "female" : "other",
   });
-}
+};
