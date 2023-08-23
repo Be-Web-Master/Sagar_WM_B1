b0dd6bd35b4aabfdd7dc614042f6e56894d4543d
isagarjaiswal <sagarjaiswal815555@gmail.com>
2023-08-23 12:31:10 +0530

    api then catch and async function

---
+++ index.js
@@ -0,0 +1,13 @@
+const fetchData = async (URL) => {
+const response = await fetch(URL);
+const data = await response.json()
+console.log(data);
+}
+fetchData("https://jsonplaceholder.typicode.com/posts")
+
+fetch("https://jsonplaceholder.typicode.com/posts")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((err) => console.log(err));
