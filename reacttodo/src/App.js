2849c116a15c9c6733cdce3cc6db05af996e9f09
isagarjaiswal <sagarjaiswal815555@gmail.com>
2023-09-09 08:11:30 +0530

    complete note making app

--- reacttodo/src/App.js
+++ reacttodo/src/App.js
@@ -1,52 +1,51 @@
 import "./App.css";
-// import React, { useState, useEffect } from "react";
-import {UseRefDemo} from "./useRefDemo"
+import React, { useState, useEffect } from "react";
+
 function App() {
-  // const [title, setTitle] = useState("");
-  // const [description, setDescription] = useState("");
-  // const [data, setData] = useState([]);
-  // const [isClear, setIsClear] = useState(false);
+  const [data, setData] = useState([]);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-  // useEffect(() => {
-  //   const getTodoData = JSON.parse(localStorage.getItem("todoData")) ?? [];
-  //   setData(getTodoData);
-  // }, []);
+  useEffect(() => {
+    const getTodoData = JSON.parse(localStorage.getItem("todoData")) ?? [];
+    setData(getTodoData);
+  }, []);
 
-  // const submitHandler = () => {
-  //   if (!title || !description) return;
-  //   const currentData = [{ title, description }, ...data];
-  //   setData(currentData);
-  //   localStorage.setItem("todoData", JSON.stringify(currentData));
-  //   setDescription("");
-  //   setTitle("");
-  // };
+  const submitHandler = () => {
+    if (!title || !description) return;
+    const currentData = [{ title, description }, ...data];
+    setData(currentData);
+    localStorage.setItem("todoData", JSON.stringify(currentData));
+    setDescription("");
+    setTitle("");
+  };
 
-  // const deleteItem = (idx) => {
-  //   const updateData = data.filter((_, id) => idx !== id);
-  //   setData(updateData);
-  //   localStorage.setItem("todoData", JSON.stringify(updateData));
-  // };
-  // const updateItem = (todo, idx) => {
-  //   setDescription(todo.description);
-  //   setTitle(todo.title);
-  //   setData((arr) => {
-  //     return arr.filter((_, id) => idx !== id);
-  //   });
-  // };
-  // const searchItem =(val) =>{
-  //   const searchData = data.filter((obj)=>{
-  //     return val === obj.title || obj.description
-  //     // console.log({val,obj});
-  //   })
-  //   setData(searchData)
-  //   console.log(searchData);
-  // }
+  const deleteItem = (idx) => {
+    const updateData = data.filter((_, id) => idx !== id);
+    setData(updateData);
+    localStorage.setItem("todoData", JSON.stringify(updateData));
+  };
+  const updateItem = (todo, idx) => {
+    setDescription(todo.description);
+    setTitle(todo.title);
+    setData((arr) => {
+      return arr.filter((_, id) => idx !== id);
+    });
+  };
+  const searchItem = (searchValue) => {
+    const data = JSON.parse(localStorage.getItem("todoData")) ?? [];
+    const searchData = data.filter(
+      (obj) =>
+        obj.title.includes(searchValue) || obj.description.includes(searchValue)
+    );
+
+    setData(searchData);
+  };
   return (
     <div className="App">
-      <UseRefDemo/>
-      {/* <h1>React Note-taking App</h1>
-      <label htmlfor>Search Here</label>
-     <input type="search"  onChange={(e)=>searchItem(e.target.value)}/>
+      <h1>React Note-taking App</h1>
+      <label htmlfor>Search Here &nbsp;</label>
+      <input type="search" onChange={(e) => searchItem(e.target.value)} />
       <form onSubmit={(e) => e.preventDefault()}>
         <label htmlfor="title">Title</label>
         <input
@@ -75,7 +74,6 @@ function App() {
         </button>
       </form>
       <hr />
-
       <div className="cards">
         {data.map((todo, idx) => {
           return (
@@ -87,7 +85,7 @@ function App() {
             </div>
           );
         })}
-      </div> */}
+      </div>
     </div>
   );
 }
