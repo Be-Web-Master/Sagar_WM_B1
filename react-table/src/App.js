3f61cda35edb0b68683e29b70fb95efa2c0cc1bb
isagarjaiswal <sagarjaiswal815555@gmail.com>
2023-09-25 11:23:19 +0530

    table uncomplete

---
+++ react-table/src/App.js
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import TableState from "./tableState";
+const App = () => {
+  
+  return (
+    <>
+      <div className="app">
+        <TableState />
+      </div>
+    </>
+  );
+};
+
+export default App;
