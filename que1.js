594a9c649d22779748a047653bd16febcfc543bd
isagarjaiswal <sagarjaiswal815555@gmail.com>
2023-09-25 12:28:26 +0530

    que 1

--- que1.js
+++ que1.js
@@ -8,10 +8,21 @@ let str = "webmaster";
 
 
 
+// const firstLetterUpperCase= (str)=>{
+//    let arr = [...str];
+//    let first = arr[0].toUpperCase()
+//    arr[0]=first
+//    return arr.join("")
+// }
+// console.log(firstLetterUpperCase(str));
+
+
 const firstLetterUpperCase= (str)=>{
-   let arr = [...str];
-   let first = arr[0].toUpperCase()
-   arr[0]=first
-   return arr.join("")
-}
-console.log(firstLetterUpperCase(str));
+    let arr = [...str];
+    let first = arr.shift().toUpperCase()
+     arr.unshift(first)
+     return arr.join("")
+
+ }
+ console.log(firstLetterUpperCase(str));
+
