20577f1411c3e80f547989ee231f79d6cc7881aa
isagarjaiswal <sagarjaiswal815555@gmail.com>
2023-09-25 12:57:37 +0530

    question 3

--- que3.js
+++ que3.js
@@ -1,6 +1,12 @@
 console.log("Question 3");
+// Reverse an array. [1,2,3,4,5,6] -> [6,5,4,3,2,1] (do not use built-in method)
 
+const reverseArray = (arr) => {
+  let brr = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    brr.push(arr[i]);
+  }
+  return brr;
+};
 
-const fun = ()=>{
-
-}
+console.log(reverseArray([1, 2, 3, 4, 5, 6]));
