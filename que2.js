af3961774aa1748b7394b8220db76f834b6d49c5
isagarjaiswal <sagarjaiswal815555@gmail.com>
2023-09-25 12:38:36 +0530

    question 2

--- que2.js
+++ que2.js
@@ -1,6 +1,14 @@
 console.log("Question 2");
+// "2. Find if the given number exists in an array - ( do not use built-in function ) [11,34,22,21,5,22] -> find 21 if exists return true otherwise false
 
+const myFind = (arr, findValue) => {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === findValue) {
+      return true;
+    }
+  }
+  return false
+};
+let arr =  [11, 34, 22, 21, 5, 22]
 
-const fun = ()=>{
-
-}
+console.log(myFind(arr, 21));
