efbf4d37efb46b5153238d8a33563bb9bfe557d2
isagarjaiwal <sagarjaiswal815555@gmail.com>
2023-10-04 12:27:40 +0530

    all question are completed

--- index4.js
+++ index4.js
@@ -6,23 +6,16 @@
 
 function findDublicatesFrequency(arr) {
   const obj = {};
-  let brr = [];
-  let count = 0;
   for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length; j++) {
-      if (arr[i] !== arr[j]) {
-        brr.push(arr[i]);
-      }
-    }
+    if (obj[arr[i]]) {
+      obj[arr[i]]++;
+    } else obj[arr[i]] = 1;
   }
-  console.log(brr);
-  //     for (let i = 0; i < arr.length; i++) {
-  //         if (obj[arr[i]]) {
-  //             obj[arr[i]]++
-  //         } else obj[arr[i]]=1
-  //     }
+  for (const [key, value] of Object.entries(obj)) {
+    if (value === 1) delete obj[key];
+  }
 
-  // return obj
+  return obj;
 }
 
 const arr = [1, 1, 2, 3, 3, 3, 4, 5, 5, 5, 5];
