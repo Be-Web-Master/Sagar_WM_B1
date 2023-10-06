adf8acfe2bd8d17fb717d4889362fd7c4f60fd65
isagarjaiwal <sagarjaiswal815555@gmail.com>
2023-10-06 11:43:52 +0530

    only Question 3

---
+++ index3.js
@@ -0,0 +1,11 @@
+// Find the square root of a given number - do not use built in method
+// const num = 16
+// output - 4
+// function getSqrt(num) - this function will return square root of a number
+
+function getSqrt(num){
+   return (num **(1/2))
+}
+const num = 16
+// output - 4
+console.log(getSqrt(num));
