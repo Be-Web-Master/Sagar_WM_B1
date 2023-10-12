08de60888bbe45db0f4d0a34c39c1d969f8ae140
isagarjaiwal <sagarjaiswal815555@gmail.com>
2023-10-12 11:33:06 +0530

    first second

---
+++ index1.js
@@ -0,0 +1,27 @@
+// Write a function to find dublicates elements without using an set and map, do it O(n), do not use nested loops
+// const arr = [1,1,2,3,4,4,4,5,6,6,7]
+// output - [1,4,6]
+// function getDublicates(arr) - this function will return array of dublicates in O(n)
+
+function getDublicates(arr) {
+  const obj = {};
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (obj[arr[i]]) {
+      obj[arr[i]]++;
+    } else {
+      obj[arr[i]] = 1;
+    }
+  }
+  let count = 0;
+  for (const key in obj) {
+    if (obj[key] > 1) {
+      result[count]=Number(key);
+      count++
+    }
+  }
+  return result;
+}
+const arr = [1, 1, 2, 3, 4, 4, 4, 5, 6, 6, 7];
+// output - [1,4,6]
+console.log(getDublicates(arr));
---
+++ index2.js
@@ -0,0 +1,28 @@
+// Write a function to find unique elements without using an set and map, do it O(n), do not use nested loops
+// const arr = [1,1,2,3,4,4,4,5,6,6,7]
+// output - [2,3,5,7]
+// function getUniqueNumbers(arr) - this function will return array of unique numbers in O(n)
+
+
+const arr = [1,1,2,3,4,4,4,5,6,6,7]
+// output - [2,3,5,7]
+function getUniqueNumbers(arr){
+    const obj = {};
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (obj[arr[i]]) {
+      obj[arr[i]]++;
+    } else {
+      obj[arr[i]] = 1;
+    }
+  }
+  let count = 0;
+  for (const key in obj) {
+    if (obj[key] == 1) {
+      result[count]=Number(key);
+      count++
+    }
+  }
+  return result;
+}
+console.log(getUniqueNumbers(arr));
